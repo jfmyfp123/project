91a4eb88913fd09734ecc8611cbc5550a8813066
jfmyfp123 <1565476233@qq.com>
2018-03-28 09:59:54 +0800

    this is test

---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+       <h1>{{msg}}</h1>
+       <h2>我是测试页</h2>
+  </div>
+</template>
+<script>
+export default{
+    name:"HelloWorld",
+    data:function(){
+        return{
+            msg:"欢迎进入vue的世界"
+        }
+    }
+}
+</script>
+<style scoped>
+h1,h2{
+    color: #42b983;
+    font-weight: normal;
+}
+</style>
